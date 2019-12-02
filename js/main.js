8f2d46394a3b3de6b8599f5e0be78c63885c5993
Tyler <bisig1892@gmail.com>
2019-12-02 15:00:15 -0800

    added method to show shopping cart items and count in shopping cart

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 window.onload = function () {
     initBuyButtons();
+    displayNumberOfItems();
+    var cartIcon = document.querySelector('#shopping-cart');
+    cartIcon.onclick = showCartContents;
 };
 /**
  * Wire up all the "Buy" buttons
@@ -12,14 +15,20 @@ function initBuyButtons() {
     }
 }
 function buyProduct() {
-    var prod = getProduct();
+    var currBtn = this; // The 'Buy' button that was clicked
+    var prod = getProduct(currBtn);
     saveProductToCart(prod);
+    displayNumberOfItems();
+}
+function displayNumberOfItems() {
+    var numItems = ProductStorage.getNumberOfProducts();
+    var cartSpan = document.querySelector('div#shopping-cart > span');
+    cartSpan.innerHTML = numItems.toString();
 }
 /**
  * Get Product object currently selected by user
  */
-function getProduct() {
-    var currBuyBtn = this;
+function getProduct(currBuyBtn) {
     var currProdDiv = currBuyBtn.parentElement;
     var prod = new Product();
     prod.title = currProdDiv.querySelector('div.title').innerHTML;
@@ -30,9 +39,55 @@ function getProduct() {
     return prod;
 }
 function saveProductToCart(p) {
+    ProductStorage.addProduct(p);
+    return ProductStorage.getAllProducts();
+}
+function showCartContents() {
+    var displayDiv = document.getElementById('display-cart');
+    displayDiv.innerHTML = '';
+    var allProds = ProductStorage.getAllProducts();
+    for (var i = 0; i < allProds.length; i++) {
+        var prod = allProds[i];
+        var prodDiv = document.createElement('div');
+        prodDiv.classList.add('display-product');
+        var header = document.createElement('h2');
+        header.innerHTML = prod.title + " - $" + prod.price;
+        prodDiv.appendChild(header);
+        var desc = document.createElement('p');
+        desc.innerHTML = "" + prod.description;
+        prodDiv.appendChild(desc);
+        displayDiv.appendChild(prodDiv);
+    }
 }
 var Product = /** @class */ (function () {
     function Product() {
     }
     return Product;
+}());
+var ProductStorage = /** @class */ (function () {
+    function ProductStorage() {
+    }
+    // Add Product
+    ProductStorage.addProduct = function (p) {
+        var prods = this.getAllProducts();
+        prods.push(p);
+        var data = JSON.stringify(prods);
+        localStorage.setItem('prods', data);
+    };
+    /**
+     * Retrieve all Products, or creats new product array
+     */
+    ProductStorage.getAllProducts = function () {
+        var data = localStorage.getItem('prods');
+        if (data == null) {
+            return new Array();
+        }
+        return JSON.parse(data);
+    };
+    // Get number of Products
+    ProductStorage.getNumberOfProducts = function () {
+        var prods = this.getAllProducts();
+        return prods.length;
+    };
+    return ProductStorage;
 }());
--- ts/main.ts
+++ ts/main.ts
@@ -1,5 +1,9 @@
 window.onload = function() {
     initBuyButtons();
+    displayNumberOfItems();
+
+    let cartIcon = <HTMLElement>document.querySelector('#shopping-cart');
+    cartIcon.onclick = showCartContents;
 }
 
 /**
@@ -14,14 +18,23 @@ function initBuyButtons() {
 }
 
 function buyProduct() {
-    let prod = getProduct();
+    let currBtn = this; // The 'Buy' button that was clicked
+    let prod = getProduct(currBtn);
     saveProductToCart(prod);
+
+    displayNumberOfItems();
 }
+
+function displayNumberOfItems() {
+    let numItems = ProductStorage.getNumberOfProducts();
+    let cartSpan = document.querySelector('div#shopping-cart > span');
+    cartSpan.innerHTML = numItems.toString();
+}
+
 /**
  * Get Product object currently selected by user
  */
-function getProduct() {
-    let currBuyBtn = <HTMLElement>this;
+function getProduct(currBuyBtn:HTMLElement) {
     let currProdDiv = currBuyBtn.parentElement;
     let prod = new Product();
     prod.title = currProdDiv.querySelector('div.title').innerHTML;
@@ -33,5 +46,24 @@ function getProduct() {
 }
 
 function saveProductToCart(p:Product):Product[] {
+    ProductStorage.addProduct(p);
+    return ProductStorage.getAllProducts();
+}
 
+function showCartContents() {
+    let displayDiv = document.getElementById('display-cart');
+    displayDiv.innerHTML = '';
+    let allProds = ProductStorage.getAllProducts();
+    for (let i = 0; i < allProds.length; i++) {
+        let prod = allProds[i];
+        let prodDiv = document.createElement('div');
+        prodDiv.classList.add('display-product');
+        let header = document.createElement('h2');
+        header.innerHTML = `${prod.title} - $${prod.price}`;
+        prodDiv.appendChild(header);
+        let desc = document.createElement('p');
+        desc.innerHTML = `${prod.description}`;
+        prodDiv.appendChild(desc);
+        displayDiv.appendChild(prodDiv);
+    }
 }
